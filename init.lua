4e64c5dda3cf4066885ca1c9b2079c67eab147c3
Zach Bugay <Zachary.Bugay@outlook.com>
2026-01-15 07:00:31 -0500

    updates to neotree and comments

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 local mySysInfo = require("mysysinfo").new(vim.uv.os_uname())
 mySysInfo:useWindowsShell()
-print("mySysInfo.processor_architecture: " .. mySysInfo.processor_architecture)
-print(vim.print(vim.uv.os_uname()))
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- [[ Setup my options, keymaps and other custom settings. ]]
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.diagnostic")
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 require("config.lazy")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "main",
+  version = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
@@ -10,7 +11,13 @@ return {
   keys = {
     { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
   },
+  ---@module 'neo-tree'
+  ---@type neotree.Config
   opts = {
+    close_if_last_window = true,
+    popup_border_style = "NC",
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
       filtered_items = {
         visible = true,
